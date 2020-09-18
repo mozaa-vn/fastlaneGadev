951decb7901706ae59c3d9e62d82aa17a8c241db
mozaa <mry.nsth@gmail.com>
2020-09-18 17:22:25 +0700

    first commit

---
+++ fastlaneGadev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fastlaneGadev
+//
+//  Created by Nguyễn Ý on 9/18/20.
+//  Copyright © 2020 Nguyễn Ý. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fastlaneGadevTests/fastlaneGadevTests.swift
@@ -0,0 +1,34 @@
+//
+//  fastlaneGadevTests.swift
+//  fastlaneGadevTests
+//
+//  Created by Nguyễn Ý on 9/18/20.
+//  Copyright © 2020 Nguyễn Ý. All rights reserved.
+//
+
+import XCTest
+@testable import fastlaneGadev
+
+class fastlaneGadevTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
